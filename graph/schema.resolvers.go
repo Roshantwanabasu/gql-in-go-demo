73200c3c050c1593112ae9c4c870df2edbdbba19
Roshan Twanabasu <roshan.twanabasu@worldlink.com.np>
2021-11-18 16:57:44 +0545

    update router and api endpoints

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -6,23 +6,44 @@ package graph
 import (
 	"context"
 	"fmt"
+	"strconv"
 
 	"github.com/Roshantwanabasu/news-clone/graph/generated"
 	"github.com/Roshantwanabasu/news-clone/graph/model"
+	"github.com/Roshantwanabasu/news-clone/internal/links"
 )
 
-func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
+func (r *mutationResolver) CreateLink(ctx context.Context, input model.NewLink) (*model.Link, error) {
+	var link links.Link
+	link.Address = input.Address
+	link.Title = input.Title
+	linkID := link.Save()
+	return &model.Link{ID: strconv.FormatInt(linkID, 10), Title: link.Title, Address: link.Address}, nil
+}
+
+func (r *mutationResolver) CreateUser(ctx context.Context, input model.NewUser) (string, error) {
 	panic(fmt.Errorf("not implemented"))
 }
 
-func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
+func (r *mutationResolver) Login(ctx context.Context, input model.Login) (string, error) {
 	panic(fmt.Errorf("not implemented"))
 }
 
-// Mutation returns generated.MutationResolver implementation.
+func (r *mutationResolver) RefreshToken(ctx context.Context, input model.RefreshTokenInput) (string, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *queryResolver) Links(ctx context.Context) ([]*model.Link, error) {
+	var resultLinks []*model.Link
+	dbLinks := links.GetAll()
+	for _, links := range dbLinks {
+		resultLinks = append(resultLinks, &model.Link{ID: links.ID, Title: links.Title, Address: links.Address})
+	}
+	return resultLinks, nil
+}
+
 func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
 
-// Query returns generated.QueryResolver implementation.
 func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
 
 type mutationResolver struct{ *Resolver }
--- server.go
+++ server.go
@@ -9,21 +9,27 @@ import (
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/Roshantwanabasu/news-clone/graph"
 	"github.com/Roshantwanabasu/news-clone/graph/generated"
+	database "github.com/Roshantwanabasu/news-clone/internal/pkg/db/migrations/mysql"
+	"github.com/go-chi/chi/v5"
 )
 
-const defaultPort = "8080"
+const defaultPort = "8000"
 
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
 	}
+	router := chi.NewRouter()
+
+	database.InitDB()
+	database.Migrate()
 
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	router.Handle("/query", srv)
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
